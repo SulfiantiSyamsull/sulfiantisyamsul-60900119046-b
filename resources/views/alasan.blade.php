a510aada84e6e12428387eba92ea8322cc1fb850
LAPTOP-GIUH8V1K\LENOVO <rmlh1405@gmail.com>
2021-10-26 03:58:42 +0800

    selamat siang

---
+++ resources/views/alasan.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+<h1>
+    <div style="text-align: center;">
+    <b>Alasan Belajar Kakas</b>
+    </h1>
+    <h3>Supaya Paham Penggunaan VCS, dalam pengembangan perangkat lunak</h3>
+</body>
+</html>
