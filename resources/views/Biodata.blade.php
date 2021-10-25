7bb2a01d0a5370305ae8745b3552269e44e0790a
LAPTOP-GIUH8V1K\LENOVO <rmlh1405@gmail.com>
2021-10-26 03:55:23 +0800

    selamat pagi

---
+++ resources/views/Biodata.blade.php
@@ -0,0 +1,23 @@
+<html>
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h1 align="center">BIODATA DIRI</h1>
+       
+            <td>Nama</td>
+            <td>Sulfianti Syamsul</td>
+       
+            <td>Kelas</td>
+            <td>B</td>
+        
+            <td>NIM</td>
+            <td>60900119034</td>
+        
+            <td>Email</td>
+            <td>60900119034@uin-alauddin.ac.id</td>
+</body>
+
+</html>
+
+</html>
